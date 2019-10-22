89e1996ed4d8f169df5d754375c90d5510a2054b
Spenser <spenser@SpensersMacBook.cable.rcn.com>
2019-10-22 17:10:05 -0400

    finally got the text to hide.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,8 @@
 // var mainNav = document.querySelectorAll('.siteNav li a');
 
-// console.log(event.target);
-
 // document.addEventListener('click', Active);
-
 //break
 // let link = document.querySelectorAll('.siteNav ul li a');
-
 // document.addEventListener('click', makeActive);
 
 // function makeActive() {
@@ -14,7 +10,22 @@
 //   makeInactive();
 //   event.target.classList.add('active');
 // }
-
 // function makeInactive() {
 //   link.forEach(link => link.classList.remove('active'));
 // }
+
+//Break
+var hiddenText = document.querySelectorAll('.readMore');
+var readMoreBtn = document.querySelector('button');
+
+function unHide() {
+  if (event.target.matches('button')) {
+    console.log('I heard you.');
+    for (let i = 0; i < hiddenText.length; i++) {
+      hiddenText[i].classList.toggle('readMore');
+    }
+    readMoreBtn.innerHTML = 'Read Less';
+  }
+}
+
+readMoreBtn.addEventListener('click', unHide);
